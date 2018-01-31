311cf048c0022a5c6af01f58277ac2b783952119
kasadawa <kasadawa@kasadawas-MacBook-Pro.local>
2018-01-31 22:14:44 +0200

    fixing server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/',express.static(path.join('public/home.html')))
+app.use(express.static('public'))
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
