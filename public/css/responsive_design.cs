253967a82c50b27fa59d1f8666cd14a041fda0f2
DESKTOP-2RFADKC\Stefan Ivanov <stefan.ivanov262@gmail.com>
2018-01-31 21:57:02 +0200

    Add css

---
+++ public/css/responsive_design.cs
@@ -0,0 +1,46 @@
+/* #### Tablets  and phones Portrait or Landscape #### */
+@media screen and (min-width: 0px) and (max-width: 1024px) {
+    .menuItem {
+        float: none;
+        font-weight: bold;
+        font-size: 25px;
+        display: block;
+        width: 100%;
+        margin: 0 auto;
+        padding:5px;
+        -moz-border-radius: 60px / 40px;
+        -webkit-border-radius: 60px / 40px;
+        border-radius: 60px / 40px;
+        background: transparent;
+        cursor: pointer;
+        text-align: center;
+        border-bottom: solid;
+        border-color: #5F5F5F;
+    }
+
+    .galleryContent{
+        height:	300px;
+        width:80%;
+        margin:10%;
+        position: relative;
+        min-width:400px;
+        display:block;
+        align-items: center;
+    }
+
+    .movie-container{
+        margin:40px 15%;
+        width: 70%;
+    }
+	
+/*	.single-movie-left{
+		justify-content:center;
+		display:block;
+		width:100%;
+		float: none;
+	}
+	.single-movie-left .rating{
+		display:block;
+		margin: auto;
+	}*/
+}
